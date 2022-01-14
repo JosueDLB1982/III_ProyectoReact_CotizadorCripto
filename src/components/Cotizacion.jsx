b0c1b0348126f1e7cdd41ff8448dd23ca186bc10
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-14 19:54:08 -0300

    Dando formato a los resultados de la consulta

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -1,15 +1,49 @@
 import styled from "@emotion/styled"
 
+const Contenedor = styled.div`
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+`
+
+const Img = styled.img`
+    display: block;
+    width: 120px;
+    gap: 1rem;
+    margin: auto;
+`
+
+const Texto = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }
+`
+const Precio = styled.p`
+    font-size: 25px;
+    span {
+        font-weight: 700;
+    }
+`
+
 const Cotizacion = ({cotizacion}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = cotizacion
     return (
-        <div>
-            <p>El Precio es de: <span>{PRICE}</span></p>
-            <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-            <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-            <p>Variación Últimas 24 Horas: <span>{CHANGEPCT24HOUR}%</span></p>
-            <p>Última Actualización: <span>{LASTUPDATE}</span></p>
-        </div>
+        <Contenedor>
+            <Img
+                src={`https://cryptocompare.com/${IMAGEURL}`}
+                alt="Imagen Criptomoneda"
+            />
+
+            <div>
+                <Precio>El Precio es de: <span>{PRICE}</span></Precio>
+                <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+                <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+                <Texto>Variación Últimas 24 Horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+                <Texto>Última Actualización: <span>{LASTUPDATE}</span></Texto>
+            </div>
+        </Contenedor>
     )
 }
 

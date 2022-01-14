aea4ac7e7517dd218156087a9a952b167b84c2fd
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-14 15:52:14 -0300

    Dando formato al array que obtenemos al consultar la API para pasrlo como opciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import styled from "@emotion/styled"
 import useSelectMonedas from "../hooks/useSelectMonedas"
 import { monedas } from "../data/monedas"
@@ -23,15 +23,25 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+    const [criptos, setCriptos] = useState([])
+
     const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas) /* Las opciones vendrán del arreglo de objetos monedas */
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
 
             const respuesta = await fetch(url)
             const resultado = await respuesta.json()
-            console.log(resultado.Data)
+            const arrayCriptos = resultado.Data.map(cripto => {
+
+                const objetoCriptos = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objetoCriptos
+            })
+            setCriptos(arrayCriptos) /* la función modificadora se llenará con los datos del array que creamos */
         }
         consultarAPI()
     }, []) /* Al no pasar dependencia se ejecuta una sola vez */

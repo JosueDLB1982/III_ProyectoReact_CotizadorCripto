01494fe237ac2854972b14a416364382b5f86ed6
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-14 15:24:15 -0300

    Consultando la API de criptos elegida

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import styled from "@emotion/styled"
 import useSelectMonedas from "../hooks/useSelectMonedas"
 import { monedas } from "../data/monedas"
@@ -23,6 +24,17 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
     const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas) /* Las opciones vendrán del arreglo de objetos monedas */
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+            console.log(resultado.Data)
+        }
+        consultarAPI()
+    }, []) /* Al no pasar dependencia se ejecuta una sola vez */
 
     return (
         <form>

f217a4ff228015b8f65ef54a5c60f844ccb1033f
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-14 17:24:11 -0300

    Detectando los valores seleccionados por el usuario - Moneda y Criptomoneda

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useLayoutEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './components/Formulario'
 import ImagenCripto from './img/imagen-criptos.png'
@@ -40,6 +40,13 @@ const Heading = styled.h1`
 `
 
 function App() {
+  const [monedas, setMonedas] = useState({})
+
+  useEffect(() => {
+    if(Object.keys(monedas).length > 0) {
+      
+    }
+  }, [monedas])
 
   return (
     <Contenedor>
@@ -51,7 +58,9 @@ function App() {
 
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
-        <Formulario/>
+        <Formulario
+          setMonedas={setMonedas}
+        />
       </div>
 
     </Contenedor>
